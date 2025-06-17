253e417b3c4596a1f8c778a5b890e11bd7cf10ef
Duerda <Eduarda.teixeira13@etec.sp.gov.br>
2025-06-17 10:02:17 -0300

    Update alunos.js

--- js/alunos.js
+++ js/alunos.js
@@ -1,18 +1,19 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCa-Tj46HGm5IkeIwPj5HrEkTrPiC5CloI",
-  authDomain: "jquery-816ed.firebaseapp.com",
-  databaseURL: "https://jquery-816ed-default-rtdb.firebaseio.com",
-  projectId: "jquery-816ed",
-  storageBucket: "jquery-816ed.firebasestorage.app",
-  messagingSenderId: "266132617291",
-  appId: "1:266132617291:web:a8247d1b631ac1ec8bf5ce",
-  measurementId: "G-LHVYV58S4V"
+  apiKey: "AIzaSyAQXcDs-sjnO4fYj8mrD7BDZp6kPW5DGMo",
+  authDomain: "jquery-firebase-4987e.firebaseapp.com",
+  databaseURL: "https://jquery-firebase-4987e-default-rtdb.firebaseio.com",
+  projectId: "jquery-firebase-4987e",
+  storageBucket: "jquery-firebase-4987e.appspot.com",
+  messagingSenderId: "280908793283",
+  appId: "1:280908793283:web:9b2e0de94c701c7b222976"
+};
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database().ref("alunos");
+
+const dbAlunos = firebase.database().ref("alunos");
 
 function carregarAlunos() {
-  db.on("value", (snapshot) => {
+  dbAlunos.on("value", (snapshot) => {
     const tbody = $("#tabelaAlunos");
     tbody.empty();
     snapshot.forEach((child) => {
@@ -24,12 +25,8 @@ function carregarAlunos() {
           <td>${aluno.email}</td>
           <td>${aluno.telefone}</td>
           <td>
-            <button class="btn btn-warning btn-sm edit-btn" data-id="${key}">
-              <i class="bi bi-pencil-fill"></i>
-            </button>
-            <button class="btn btn-danger btn-sm delete-btn" data-id="${key}">
-              <i class="bi bi-trash-fill"></i>
-            </button>
+            <button class="btn btn-warning btn-sm edit-btn" data-id="${key}">Editar</button>
+            <button class="btn btn-danger btn-sm delete-btn" data-id="${key}">Excluir</button>
           </td>
         </tr>
       `);
@@ -39,22 +36,25 @@ function carregarAlunos() {
 
 $("#formAluno").submit(function (e) {
   e.preventDefault();
+
   const id = $("#id").val();
   const nome = $("#txtnome").val();
   const email = $("#txtemail").val();
   const telefone = $("#txttelefone").val();
+
   if (id) {
-    db.child(id).update({ nome, email, telefone });
+    dbAlunos.child(id).update({ nome, email, telefone });
   } else {
-    db.push({ nome, email, telefone });
+    dbAlunos.push({ nome, email, telefone });
   }
+
   this.reset();
   $("#id").val("");
 });
 
 $(document).on("click", ".edit-btn", function () {
   const id = $(this).data("id");
-  db.child(id).get().then((snapshot) => {
+  dbAlunos.child(id).get().then((snapshot) => {
     const aluno = snapshot.val();
     $("#id").val(id);
     $("#txtnome").val(aluno.nome);
@@ -65,8 +65,8 @@ $(document).on("click", ".edit-btn", function () {
 
 $(document).on("click", ".delete-btn", function () {
   const id = $(this).data("id");
-  if (confirm("Tem certeza que deseja excluir este aluno?")) {
-    db.child(id).remove();
+  if (confirm("Deseja excluir este aluno?")) {
+    dbAlunos.child(id).remove();
   }
 });
 
